7525570834763ef67f8d89e8dc5a59d9a6305895
“czgrzegorz <cz.grzegorz@gmail.com>
2017-12-04 17:35:14 +0100

    Pierwsza proba

---
+++ src/main/java/com/kodilla/MethodsForCalculation.java
@@ -0,0 +1,15 @@
+package com.kodilla;
+
+public class MethodsForCalculation {
+
+    public double doAddition(double a, double b) {
+        double c = a+b;
+        return c;
+        System.out.println(c);
+    }
+    public double doSubstraction(double a, double b) {
+        double d = a-b;
+        return d;
+        System.out.println(d);
+    }
+}
